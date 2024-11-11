c1c36b894af55adf22e5c30efee578f65a1d937c
shubham <shubhamborate25@gmail.com>
2024-11-11 12:39:12 +0530

    commits

--- src/main/java/com/ContactManager/Entity/User.java
+++ src/main/java/com/ContactManager/Entity/User.java
@@ -32,7 +32,7 @@ public class User {
 	@Size(min = 6, message = "Minimum 6 character !!")
 	private String password;
 	private String role; 
-	private boolean enable;
+	private boolean enable = true ;
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
 	private List<Contact> contact;
 
